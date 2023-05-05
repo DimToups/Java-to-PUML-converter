70bfa40addfa64e471730100e5d9a6c1318fbe6f
Dimitri.T <dim.saesso@gmail.com>
2023-05-05 15:11:56 +0200

    [Semaine_1] Ajout du projet western en tant que sujet de test

--- src/pumlFromJava/Java2Puml.java
+++ src/pumlFromJava/Java2Puml.java
@@ -1,19 +1,15 @@
 package pumlFromJava;
 
 import java.util.spi.ToolProvider;
-
 public class Java2Puml
 {
-
     public static void main(String[] args)
     {
         ToolProvider toolProvider = ToolProvider.findFirst("javadoc").get();
         System.out.println(toolProvider.name());
 
-/*
-    javadoc -private -sourcepath <src> -doclet pumlFromJava.FirstDoclet -docletpath out/production/<projet>
-      <package> ... <fichiers>
- */
+        //D:\Programmes\JDK\bin\javadoc.exe -private -sourcepath p21_projet/src/western/src -doclet pumlFromJava.FirstDoclet -docletpath out/production/SAE21 western
+
         toolProvider.run(System.out, System.err, args);
     }
 }
