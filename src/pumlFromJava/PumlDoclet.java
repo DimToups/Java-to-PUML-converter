51c027fa362e1dccab4979ee7b83f05cd0b63441
Dimitri.T <dim.saesso@gmail.com>
2023-05-11 12:16:14 +0200

    [Semaine2] Finalisation des options du doclet

--- src/pumlFromJava/PumlDoclet.java
+++ src/pumlFromJava/PumlDoclet.java
@@ -19,38 +19,6 @@ public class PumlDoclet implements Doclet{
     @Override
     public Set<? extends Option> getSupportedOptions() {
         Set<Option> options = new HashSet<>();
-        //-out
-        options.add(
-                new Option() {
-                    @Override
-                    public int getArgumentCount() {
-                        return 1;
-                    }
-                    @Override
-                    public String getDescription() {
-                        return "Fixe le nom du fichier créé.";
-                    }
-                    @Override
-                    public Kind getKind() {
-                        return Kind.OTHER;
-                    }
-                    @Override
-                    public List<String> getNames() {
-                        List<String> names = new ArrayList<>();
-                        names.add("-out");
-                        return names;
-                    }
-                    @Override
-                    public String getParameters() {
-                        return "-out <p1>";
-                    }
-                    @Override
-                    public boolean process(String s, List<String> list) {
-                        
-                        return true;
-                    }
-                }
-        );
         //-d
         options.add(
                 new Option() {
@@ -83,6 +51,40 @@ public class PumlDoclet implements Doclet{
 
                     @Override
                     public boolean process(String s, List<String> list) {
+                        directory = list.get(0);
+                        return true;
+                    }
+                }
+        );
+        //-out
+        options.add(
+                new Option() {
+                    @Override
+                    public int getArgumentCount() {
+                        return 1;
+                    }
+                    @Override
+                    public String getDescription() {
+                        return "Fixe le nom du fichier créé.";
+                    }
+                    @Override
+                    public Kind getKind() {
+                        return Kind.STANDARD;
+                    }
+                    @Override
+                    public List<String> getNames() {
+                        List<String> names = new ArrayList<>();
+                        names.add("-out");
+                        return names;
+                    }
+                    @Override
+                    public String getParameters() {return "-out <p1>";}
+                    @Override
+                    public boolean process(String s, List<String> list) {
+                        if (!list.get(0).endsWith(".puml"))
+                            name = list.get(0) + ".puml";
+                        else
+                            name = list.get(0);
                         return true;
                     }
                 }
@@ -95,7 +97,9 @@ public class PumlDoclet implements Doclet{
     }
     @Override
     public boolean run(DocletEnvironment docletEnvironment) {
+        PumlDiagram diagram = new PumlDiagram();
 
         return true;
     }
 }
+//-out westernClass.puml
