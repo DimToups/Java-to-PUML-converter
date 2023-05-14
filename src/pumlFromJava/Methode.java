ab0a9a78305f2c5a66dcbe67d775a932d0d69fbd
Dimitri.T <dim.saesso@gmail.com>
2023-05-14 15:35:53 +0200

    [Semaine2] Finalisation du DCA et du DCC de l'API pumlFromJava

--- src/pumlFromJava/Methode.java
+++ src/pumlFromJava/Methode.java
@@ -4,8 +4,8 @@ import javax.lang.model.type.PrimitiveType;
 
 public class Methode {
     String nom;
-    PrimitiveType type = null;
-    Visibilite visibilite = null;
+    PrimitiveType type;
+    Visibilite visibilite;
     public Methode(String nom){
         this.nom = nom;
     }
