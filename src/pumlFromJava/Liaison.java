347560b55f71f4740da796a99a28413435e0f5ac
Dimitri.T <dim.saesso@gmail.com>
2023-05-16 13:43:43 +0200

    [Semaine3] Ajout d'une classe Liaison et une énumération TypeLiaison

---
+++ src/pumlFromJava/Liaison.java
@@ -0,0 +1,8 @@
+package pumlFromJava;
+
+
+public class Liaison {
+    String element1;
+    String element2;
+    TypeLiaison typeLiaison;
+}
---
+++ src/pumlFromJava/TypeLiaison.java
@@ -0,0 +1,5 @@
+package pumlFromJava;
+
+public enum TypeLiaison {
+    HERITAGE, SIMPLE, IMPLEMENT, AGREGATION, COMPOSITION
+}
