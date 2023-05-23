c8bb6863a426a7cc8e388c314e7fef4eeffdf7dc
TOUPENSE DIMITRI <dimitri.toupense@etu.unistra.fr>
2023-05-23 16:18:13 +0200

    [Semaine4] Ajout de l'énumération Modificateur et implémentation de l'énumération dans les classes Attribut et Methode

--- src/pumlFromJava/Attribut.java
+++ src/pumlFromJava/Attribut.java
@@ -1,15 +1,41 @@
 package pumlFromJava;
 
 import javax.lang.model.type.PrimitiveType;
+import javax.lang.model.type.TypeMirror;
 
 public class Attribut {
     private String nom;
-    private PrimitiveType type;
+    private TypeMirror type;
     private Visibilite visibilite;
+    private Modificateur modificateur;
     public Attribut(String nom){
         this.nom = nom;
     }
-    public String getNom(){ return this.nom;}
-    public PrimitiveType getType(){ return this.type;}
-    public Visibilite getVisibilite(){ return this.visibilite;}
+    public Attribut(String nom, TypeMirror type){
+        this.nom = nom;
+        this.type = type;
+    }
+    public Attribut(String nom, TypeMirror type, Visibilite visibilite){
+        this.nom = nom;
+        this.type = type;
+        this.visibilite = visibilite;
+    }
+    public Attribut(String nom, TypeMirror type, Modificateur modificateur){
+        this.nom = nom;
+        this.type = type;
+        this.modificateur = modificateur;
+    }
+    public Attribut(String nom, TypeMirror type, Visibilite visibilite, Modificateur modificateur){
+        this.nom = nom;
+        this.type = type;
+        this.visibilite = visibilite;
+        this.modificateur = modificateur;
+    }
+    public String getNom(){return this.nom;}
+    public TypeMirror getType(){return this.type;}
+    public Visibilite getVisibilite(){return this.visibilite;}
+    public Modificateur getModificateur(){return this.modificateur;}
+    public void setType(TypeMirror type) {this.type = type;}
+    public void setVisibilite(Visibilite visibilite) {this.visibilite = visibilite;}
+    public void setModificateur(Modificateur modificateur) {this.modificateur = modificateur;}
 }
--- src/pumlFromJava/Methode.java
+++ src/pumlFromJava/Methode.java
@@ -1,15 +1,23 @@
 package pumlFromJava;
 
 import javax.lang.model.type.PrimitiveType;
+import javax.lang.model.type.TypeMirror;
+import java.util.ArrayList;
 
 public class Methode {
     private String nom;
-    private PrimitiveType type;
+    private TypeMirror type;
     private Visibilite visibilite;
+    private Modificateur modificateur;
+    private ArrayList<Attribut> parametres;
     public Methode(String nom){
         this.nom = nom;
     }
     public String getNom(){ return this.nom;}
-    public PrimitiveType getType(){ return this.type;}
-    public Visibilite getVisibilite(){ return this.visibilite;}
+    public TypeMirror getType(){return this.type;}
+    public Visibilite getVisibilite(){return this.visibilite;}
+    public Modificateur getModificateur(){return this.modificateur;}
+    public void setType(TypeMirror type) {this.type = type;}
+    public void setVisibilite(Visibilite visibilite) {this.visibilite = visibilite;}
+    public void setModificateur(Modificateur modificateur) {this.modificateur = modificateur;}
 }
