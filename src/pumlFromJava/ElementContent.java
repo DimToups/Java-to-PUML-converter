0876f7425a3a29d46fb54e30500cabf0267ff78f
daonguyen.tran <daonguyen.tran04@gmail.com>
2023-06-08 23:00:56 +0200

    Modification du contenu des classes dans le DCC de notre API

--- src/pumlFromJava/ElementContent.java
+++ src/pumlFromJava/ElementContent.java
@@ -6,7 +6,7 @@ import javax.lang.model.element.ElementKind;
 public abstract class ElementContent {
     protected String className;
     protected ElementKind classType;
-    private int etagePuml;
+    private Integer etagePuml;
     public ElementContent(Element element){
         this.className = element.getSimpleName().toString();
         this.classType = element.getKind();
@@ -18,6 +18,6 @@ public abstract class ElementContent {
     public ElementKind getType(){
         return this.classType;
     }
-    public void setEtagePuml(int etage){this.etagePuml = etage;}
-    public int getPumlEtage(){return this.etagePuml;}
+    public void setEtagePuml(Integer etage){this.etagePuml = etage;}
+    public Integer getPumlEtage(){return this.etagePuml;}
 }
